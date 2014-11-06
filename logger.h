c8e71f90804ada1230a9a0600c86477cbf4d9276
Raffaella Migliaccio <raffaella.migliaccio@studenti.unimi.it>
2014-11-06 16:57:32 +0100

    Added license blurb

--- logger.h
+++ logger.h
@@ -1,3 +1,22 @@
+/*
+	Copyright 2014 Raffaella Migliaccio
+
+	This file is part of log4ksp.
+
+    Log4ksp is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    Log4ksp is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+    details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Log4ksp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef NDEBUG
 
 declare $_logger_idx_
